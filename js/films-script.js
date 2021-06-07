51ad10eb81c9f6c4b18a61e939cbd9a43249060e
Akyru <78698076+Akyru@users.noreply.github.com>
2021-06-08 00:47:48 +0200

    Film

--- js/films-script.js
+++ js/films-script.js
@@ -3,17 +3,24 @@ $( function(){ // chargement du DOM
       $('.owl-carousel').owlCarousel({
 
         items: 1,     // nombre d'items visibles en même temps
-        loop:true,    // si mon slider fait une boucle
+        loop:false,    // si mon slider fait une boucle
 
-        dots: true,   // pour activer les points de navigation
+        margin: 50,  // rajoute une marge à droite (en px) de chaque slide
+        dots: false,   // pour activer les points de navigation
         nav: true,    // pour activer les boutons de navigation préc/suiv
-        navText: ['<img src="img/vox-triangle.svg" alt="Flèche de gauche du slider"/>','<img src="img/vox-triangle.svg" alt="Flèche de gauche du slider"/>'],// changer le texte des boutons suivant précédent
+        navText: ['<img src="../img/vox-triangle.svg" alt="Flèche de gauche du slider"/>','<img src="../img/vox-triangle.svg" alt="Flèche de gauche du slider"/>'],// changer le texte des boutons suivant précédent
 
-        autoplay: true, // pour jouer automatiquement le slider
-        autoplayTimeout: 7000,   // durée (en ms) entre chaque slide
-        autoplaySpeed: 2000,      // temps pour passage d'une slide à l'autre
+        autoplay: false, // pour jouer automatiquement le slider
+        autoplayTimeout: 2000,   // durée (en ms) entre chaque slide
+        autoplaySpeed: 500,      // temps pour passage d'une slide à l'autre
         slideTransition: 'ease', // courbe d'évolution de ma transition (linear, etc...)
         autoplayHoverPause: true,  // si mon slider s'arrête quand je le survole
+
+        responsive: { // choisir d'autres options en fonction des breakpoints
+        // breakpoint : { option1: valeur, option2: valeur, option3: valeur }
+            720   : { items: 2, slideBy: 2 }, // si j'affiche 2 items, je slide par 2
+            960   : { items: 4, slideBy: 4 }
+          }
 
     }); // fin de Carousel
 
